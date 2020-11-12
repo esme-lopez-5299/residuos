dfe1568c1d167f77b7c1dcd87b338e23d81ef142
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-11-11 18:43:25 -0600

    Avance controller

--- RESIDUOS/RESIDUOS/Controllers/HomeController.cs
+++ RESIDUOS/RESIDUOS/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RESIDUOS.Models;
+using RESIDUOS.Repositories;
 
 namespace RESIDUOS.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController(residuosContext ctx)
+        {
+            Context = ctx;
+        }
+
+        residuosContext Context;
         public IActionResult Index()
         {
             return View();
@@ -25,6 +33,15 @@ namespace RESIDUOS.Controllers
 
         public IActionResult Registrarse()
         {
+            Usuario user = new Usuario();
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Registrarse(Usuario user)
+        {
+            Repository<Usuario> repos = new Repository<Usuario>(Context);
+            repos.Insert(user);
             return View();
         }
 
---
+++ RESIDUOS/RESIDUOS/Repositories/UsuarioRepository.cs
@@ -0,0 +1,22 @@
+﻿using RESIDUOS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace RESIDUOS.Repositories
+{
+    public class UsuarioRepository : Repository<Usuario>
+    {
+        public UsuarioRepository(residuosContext ctx) : base(ctx)
+        {
+            
+        }
+
+        //public override void Insert(Usuario entidad)
+        //{
+            
+        //}
+    }
+}
